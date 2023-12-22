3135268951602d20a3ef8e876f353fdbd9c5e54f
halidak <halida.karisik6@gmail.com>
2023-12-22 19:05:28 +0100

    varcel files

---
+++ build_files.sh
@@ -0,0 +1,5 @@
+pip install django
+pip install djangorestframework
+pip install django-cors-headers
+
+python3.9 manage.py collectstatic
--- pytnikApi/settings.py
+++ pytnikApi/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-s%_##v4=hg)3km^gc279xn($67o148@bq70j7z%9e^-b8%#4l%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.varcel.app', '.now.sh']
 
 
 # Application definition
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR , 'static')
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
